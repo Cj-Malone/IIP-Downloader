fd80342a7b6d8d73a4a35534cbf39cf04bddf578
Cj Malone <CjMalone@mail.com>
2020-10-14 20:13:53 +0100

    Make script resumable
    if you stop it at the right time

--- download.sh
+++ download.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
 while IFS="," read -r name filename url; do
-	echo "$name"
-	wget "$url" --output-document="data/$filename"
 	dirname="${filename:0:-4}"
-	unzip "data/$filename" -d "data/$dirname"
-	ogr2ogr -f "ESRI Shapefile" "data/$dirname.shp" "data/$dirname/Land_Registry_Cadastral_Parcels.gml"
+
+	echo "$name"
+
+	if [ -f "data/$filename" ]; then
+		echo "Skipping download"
+	else
+		wget "$url" --output-document="data/$filename"
+	fi
+	if [ -d "data/$dirname" ]; then
+		echo "Skipping extraction"
+	else
+		unzip "data/$filename" -d "data/$dirname"
+	fi
+	if [ -f "data/$dirname.shp" ]; then
+		echo "Skipping conversion"
+	else
+		ogr2ogr -f "ESRI Shapefile" "data/$dirname.shp" "data/$dirname/Land_Registry_Cadastral_Parcels.gml"
+	fi
 done < "local authorities.csv"
 
