7e42b8b219fc8d5d9975a306829ff4393e4d8f29
Cj Malone <CjMalone@mail.com>
2020-10-14 19:41:23 +0100

    Convert gml to shape file

--- download.sh
+++ download.sh
@@ -5,5 +5,6 @@ while IFS="," read -r name filename url; do
 	wget "$url" --output-document="data/$filename"
 	dirname="${filename:0:-4}"
 	unzip "data/$filename" -d "data/$dirname"
+	ogr2ogr -f "ESRI Shapefile" "data/$dirname.shp" "data/$dirname/Land_Registry_Cadastral_Parcels.gml"
 done < "local authorities.csv"
 
