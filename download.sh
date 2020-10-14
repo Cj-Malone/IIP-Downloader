b7eece8f929641d23733b0b75a5c708f2b7150b1
Cj Malone <CjMalone@mail.com>
2020-10-14 19:32:48 +0100

    unzip archive

--- download.sh
+++ download.sh
@@ -3,5 +3,7 @@
 while IFS="," read -r name filename url; do
 	echo "$name"
 	wget "$url" --output-document="data/$filename"
+	dirname="${filename:0:-4}"
+	unzip "data/$filename" -d "data/$dirname"
 done < "local authorities.csv"
 
