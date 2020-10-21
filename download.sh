ca485319022a113e40b269ff367e5e3cc71998fd
RobJN <rob.j.nickerson@gmail.com>
2020-10-21 23:01:22 +0100

    Process for Scotland and speed up by not unzipping (#4)
    Process Scottish data and don't unzip
    
    ogr2ogr line to process Scottish data and speeding up the script by not unzipping. We use the virtual file system (/vsizip/)

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 mkdir --parents "data"
 
 while IFS="," read -r name filename url; do
-	dirname="${filename:0:-4}"
+	shortname="${filename:0:-4}"
 
 	echo "$name"
 
@@ -11,24 +11,19 @@ while IFS="," read -r name filename url; do
 		echo "Skipping download"
 	else
 		wget "$url" --output-document="data/$filename"
-	fi
-	if [ -d "data/$dirname" ]; then
-		echo "Skipping extraction"
-	else
-		unzip "data/$filename" -d "data/$dirname"
-	fi
-	if [ -f "data/$dirname.imported" ]; then
+	fi	
+	if [ -f "data/$shortname.imported" ]; then
 		echo "Skipping conversion"
 	else
-		if [ -f "data/$dirname/INSPIRE Download Licence.pdf" ]; then
-			# England datasets
-			cp Land_Registry_Cadastral_Parcels.gfs "data/$dirname/Land_Registry_Cadastral_Parcels.gfs"
-			ogr2ogr -append -f SQLite -dialect SQLite -sql "SELECT geometry FROM PREDEFINED" combined.sqlite "data/$dirname/Land_Registry_Cadastral_Parcels.gml"
-			touch "data/$dirname.imported"
-		else
+		if [[ $shortname == [A-Z][A-Z][A-Z] ]]; then
 			# Scotland datasets
-			echo "TODO: Skipping Scottish dataset"
+			ogr2ogr -append -f SQLite -dialect SQLite -sql "SELECT geometry FROM ${shortname}_bng" combined.sqlite "/vsizip/data/$filename/${shortname}_bng.shp"
+			touch "data/$shortname.imported"
+		else
+			# England & Wales datasets
+			zip -ru "data/$filename" Land_Registry_Cadastral_Parcels.gfs
+			ogr2ogr -append -f SQLite -dialect SQLite -sql "SELECT geometry FROM PREDEFINED" combined.sqlite "/vsizip/data/$filename/Land_Registry_Cadastral_Parcels.gml"
+			touch "data/$shortname.imported"
 		fi
 	fi
 done < "local authorities.csv"
-
